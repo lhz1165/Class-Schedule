638ed775c528a360629b457cac42e744fc505581
lhz <2190732276@qq.com>
2021-05-06 13:11:55 +0800

    es demo

---
+++ src/main/java/com/lhz/estest/config/ElasticSearchConfig.java
@@ -0,0 +1,67 @@
+package com.lhz.estest.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+
+/**
+ * @Author: cxw
+ * @CreateDate: 2019-05-23 14:22
+ * @Description:
+ */
+@Slf4j
+@Configuration
+public class ElasticSearchConfig {
+
+
+    private String hostName="localhost";
+
+    /**
+     * 端口
+     */
+
+    private String port="9300";
+
+    /**
+     * 集群名称
+     */
+    private String clusterName="elasticsearch";
+
+    /**
+     * 连接池
+     */
+    private String poolSize="5";
+
+    /**
+     * Bean name default  函数名字
+     * @return
+     */
+    @Bean(name = "transportClient")
+    public TransportClient transportClient() {
+        log.info("Elasticsearch初始化开始。。。。。");
+        TransportClient transportClient = null;
+        try {
+            // 配置信息
+            Settings esSetting = Settings.builder()
+                    .put("cluster.name", clusterName) //集群名字
+                    .put("client.transport.sniff", true)//增加嗅探机制，找到ES集群
+                    .put("thread_pool.search.size", Integer.parseInt(poolSize))//增加线程池个数，暂时设为5
+                    .build();
+            //配置信息Settings自定义
+            transportClient = new PreBuiltTransportClient(esSetting);
+            TransportAddress transportAddress = new TransportAddress(InetAddress.getByName(hostName), Integer.valueOf(port));
+            transportClient.addTransportAddresses(transportAddress);
+        } catch (Exception e) {
+            log.error("elasticsearch TransportClient create error!!", e);
+        }
+        return transportClient;
+    }
+
+}
---
+++ src/main/java/com/lhz/estest/dao/ElasticRepository.java
@@ -0,0 +1,25 @@
+package com.lhz.estest.dao;
+
+import com.lhz.estest.eneity.DocBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ElasticRepository extends ElasticsearchRepository<DocBean, Long> {
+
+    //默认的注释
+    //@Query("{\"bool\" : {\"must\" : {\"field\" : {\"content\" : \"?\"}}}}")
+    Page<DocBean> findByContent(String content, Pageable pageable);
+
+    @Query("{\"bool\" : {\"must\" : {\"field\" : {\"firstCode.keyword\" : \"?\"}}}}")
+    Page<DocBean> findByFirstCode(String firstCode, Pageable pageable);
+
+    @Query("{\"bool\" : {\"must\" : {\"field\" : {\"secordCode.keyword\" : \"?\"}}}}")
+    Page<DocBean> findBySecordCode(String secordCode, Pageable pageable);
+
+
+}
+
