6aa47fb9c2ff944de7b8d4cf5c37a8743b93401f
lhz <2190732276@qq.com>
2021-05-06 16:10:18 +0800

    client curd

--- src/test/java/com/lhz/estest/EsTestApplicationTests.java
+++ src/test/java/com/lhz/estest/EsTestApplicationTests.java
@@ -1,8 +1,19 @@
 package com.lhz.estest;
 
 import com.lhz.estest.dao.ElasticRepository;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.script.ScriptService;
+import org.elasticsearch.script.mustache.SearchTemplateRequestBuilder;
+import org.elasticsearch.search.SearchHit;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +25,12 @@ import java.io.IOException;
 import java.util.Date;
 
 
+import  org.elasticsearch.action.search.SearchType;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class EsTestApplicationTests {
-    @Autowired
-    private ElasticRepository elasticRepository;
+
 
     @Autowired
     private TransportClient client;
@@ -36,5 +47,36 @@ class EsTestApplicationTests {
                 )
                 .get();
     }
+    @Test
+    void search() throws IOException{
+        // 这只查询的字段和字段值，这里会进行分词查询，相当于match
+        QueryBuilder builder = QueryBuilders.matchQuery("user","kimchy");
+//        SearchResponse searchResponse = client.prepareSearch("twitter")
+//                .setSearchType(SearchType.QUERY_THEN_FETCH)
+//                .setQuery(builder)
+//                .setSize(10)
+//                .get();
+        SearchRequestBuilder srb = client.prepareSearch("twitter")
+                .setSearchType(SearchType.QUERY_THEN_FETCH)
+                .setQuery(builder)
+                .setSize(10);
+        SearchResponse searchResponse = srb.execute().actionGet();
+        SearchHit[] searchHits = searchResponse.getHits().getHits();
+        for(SearchHit searchHit : searchHits){
+            System.out.println(searchHit.getSourceAsString());
+        }
+    }
+
+    @Test
+    public void test6() throws Exception{
+        XContentBuilder doc = XContentFactory.jsonBuilder().startObject()
+                .field("user", "adsa kimchy hello")
+                .field("postDate", new Date())
+                .field("message", "trying out")
+                .endObject();
+        IndexResponse response = client.prepareIndex("twitter", "_doc","3").setSource(doc).get();
+        System.out.println(response.status());
+    }
+
 
 }
