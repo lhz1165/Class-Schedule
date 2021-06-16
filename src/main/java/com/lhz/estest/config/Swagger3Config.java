47449ed71a7bbc62f2b2829f9883ca11d621c265
lhz1165 <2190732276@qq.com>
2021-06-17 01:20:04 +0800

    整合mybatis plus

--- src/main/java/com/lhz/estest/config/Swagger3Config.java
+++ src/main/java/com/lhz/estest/config/Swagger3Config.java
@@ -1,6 +1,5 @@
 package com.lhz.estest.config;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -8,7 +7,6 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
--- src/main/java/com/lhz/estest/controller/CourseController.java
+++ src/main/java/com/lhz/estest/controller/CourseController.java
@@ -16,9 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by: hz.lai
@@ -30,72 +32,108 @@ import java.util.Map;
 @RequestMapping("/")
 public class CourseController {
 
-    @Autowired
-    private ICourseService courseService;
+	@Autowired
+	private ICourseService courseService;
 
-    @Autowired
-    private IStudentService studentService;
-
-
-    @ApiOperation("我的课程")
-    @RequestMapping(path = "/myCourse",method = RequestMethod.GET)
-    public Result myClourse(HttpSession session) {
-        String studentNo = (String) session.getAttribute("studentNo");
-        Student student = studentService.lambdaQuery().eq(Student::getUserNo, studentNo).one();
-        List<Course> myCourses = courseService.lambdaQuery().eq(Course::getStuId, student.getId()).list();
-        return Result.success(myCourses);
-    }
+	@Autowired
+	private IStudentService studentService;
 
 
-    @ApiOperation("删除课程")
-    @RequestMapping(path = "/delCourse",method = RequestMethod.GET)
-    public Result delCourse(@RequestParam Integer day, @RequestParam String startTime, HttpSession session) {
-        String studentNo = (String) session.getAttribute("studentNo");
-        Student student = studentService.lambdaQuery().eq(Student::getUserNo, studentNo).one();
-        Map<String, Object> params = new HashMap<>();
-        params.put("stu_id",student.getId());
-        params.put("start_time",startTime);
-        params.put("course_day",day);
-        courseService.removeByMap(params);
-        return Result.success();
+	@ApiOperation("我的课程")
+	@RequestMapping(path = "/myCourse", method = RequestMethod.GET)
+	public Result myClourse(HttpSession session) {
+		String studentNo = (String) session.getAttribute("studentNo");
+		Student student = studentService.lambdaQuery().eq(Student::getUserNo, studentNo).one();
+		List<Course> courses = courseService.lambdaQuery().eq(Course::getStuId, student.getId()).list();
+		Map<Integer, List<Course>> row2Courses = new HashMap<>();
+		for (int i = 1; i <= 13; i++) {
+			row2Courses.put(i, initAll());
+		}
+		Map<Integer, List<Course>> mycourse = courses.stream().collect(Collectors.groupingBy(Course::getCourseNo));
+		//表格里面每个格子都填好了 7*13的表格
+		fillCourse(row2Courses, mycourse);
+		return Result.success(row2Courses);
+	}
 
-    }
-    @ApiOperation("添加或修改课程")
-    @RequestMapping(path ="/addOrUpdateCourse",method = RequestMethod.GET)
-    public Result addCourse(@RequestParam Integer day,
-                            @RequestParam String startTime,
-                            @RequestParam String endTime,
-                            HttpSession session,
-                            @RequestParam String courseName,
-                            @RequestParam String courseAddress,
-                            @RequestParam Integer courseStartNo,
-                            @RequestParam Integer courseEndNo) {
-        String studentNo = (String) session.getAttribute("studentNo");
-        Student student = studentService.lambdaQuery().eq(Student::getUserNo, studentNo).one();
-        LambdaQueryChainWrapper<Course> wrapper = courseService.lambdaQuery()
-                .eq(Course::getStuId, student.getId())
-                .eq(Course::getStartTime, startTime)
-                .eq(Course::getEndTime, endTime)
-                .eq(Course::getCourseDay, day);
-        Course course = wrapper.one();
-        boolean isAdd = false;
-        if (course == null) {
-            course = new Course();
-            course.setStuId(student.getId());
-            isAdd = true;
-        }
-        course.setCourseLen(courseEndNo - courseStartNo);
-        course.setCourseDay(day);
-        course.setCourseDay(day);
-        course.setStartTime(startTime);
-        course.setEndTime(endTime);
-        course.setCourseNo(courseStartNo);
-        course.setCourseName(courseName);
-        course.setCourseAddress(courseAddress);
-        courseService.saveOrUpdate(course);
-        if (isAdd) {
-            Result.success("新增成功");
-        }
-        return Result.success("修改成功");
-    }
+	public List<Course> initAll() {
+		List<Course> rowAllCourse = new ArrayList<>();
+		for (int i = 0; i < 7; i++) {
+			Course course = new Course();
+			course.setCourseName("white");
+			rowAllCourse.add(course);
+		}
+		return rowAllCourse;
+	}
+
+	public void fillCourse(Map<Integer, List<Course>> row2Courses, Map<Integer, List<Course>> mycourse) {
+		for (int i = 1; i <= 13; i++) {
+			//这行（no）有课程的的列表
+			List<Course> myCourse = mycourse.get(i);
+			if (myCourse != null) {
+				for (Course course : myCourse) {
+					int len = course.getCourseLen();
+					for (int j = course.getCourseNo(); j < course.getCourseNo() + len; j++) {
+						List<Course> rowCourse = row2Courses.get(j);
+						rowCourse.set(course.getCourseDay() - 1, course);
+					}
+				}
+			}
+
+
+		}
+	}
+
+
+	@ApiOperation("删除课程")
+	@RequestMapping(path = "/delCourse", method = RequestMethod.GET)
+	public Result delCourse(@RequestParam Integer day, @RequestParam String startTime, HttpSession session) {
+		String studentNo = (String) session.getAttribute("studentNo");
+		Student student = studentService.lambdaQuery().eq(Student::getUserNo, studentNo).one();
+		Map<String, Object> params = new HashMap<>();
+		params.put("stu_id", student.getId());
+		params.put("start_time", startTime);
+		params.put("course_day", day);
+		courseService.removeByMap(params);
+		return Result.success();
+
+	}
+
+	@ApiOperation("添加或修改课程")
+	@RequestMapping(path = "/addOrUpdateCourse", method = RequestMethod.GET)
+	public Result addCourse(@RequestParam Integer day,
+							@RequestParam String startTime,
+							@RequestParam String endTime,
+							HttpSession session,
+							@RequestParam String courseName,
+							@RequestParam String courseAddress,
+							@RequestParam Integer courseStartNo,
+							@RequestParam Integer courseEndNo) {
+		String studentNo = (String) session.getAttribute("studentNo");
+		Student student = studentService.lambdaQuery().eq(Student::getUserNo, studentNo).one();
+		LambdaQueryChainWrapper<Course> wrapper = courseService.lambdaQuery()
+				.eq(Course::getStuId, student.getId())
+				.eq(Course::getStartTime, startTime)
+				.eq(Course::getEndTime, endTime)
+				.eq(Course::getCourseDay, day);
+		Course course = wrapper.one();
+		boolean isAdd = false;
+		if (course == null) {
+			course = new Course();
+			course.setStuId(student.getId());
+			isAdd = true;
+		}
+		course.setCourseLen(courseEndNo - courseStartNo);
+		course.setCourseDay(day);
+		course.setCourseDay(day);
+		course.setStartTime(startTime);
+		course.setEndTime(endTime);
+		course.setCourseNo(courseStartNo);
+		course.setCourseName(courseName);
+		course.setCourseAddress(courseAddress);
+		courseService.saveOrUpdate(course);
+		if (isAdd) {
+			Result.success("新增成功");
+		}
+		return Result.success("修改成功");
+	}
 }
